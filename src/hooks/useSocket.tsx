2d63ba4690ee029ff18c4cfa1d86e95c257fc667
priyanshpatel18 <priyansh.180804@gmail.com>
2025-03-24 03:39:05 +0530

    shifted ws to env

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-const WS_URL = "ws://localhost:8080";
+const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
 
 export function useSocket() {
   const socketRef = useRef<WebSocket | null>(null);
