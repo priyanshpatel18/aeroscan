506f420c084c5457595b4e10d1f62e1cd06adcb2
prianshpate18 <priyansh.180804@gmail.com>
2025-09-10 16:57:01 +0530

    fix: solved dependency error

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
-  // Windows-specific fixes
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
